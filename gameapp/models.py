72d299b7e4eb0ef35f6cc2088e34648f75d8de3a
birbdadaddy <108766045+birbdadaddy@users.noreply.github.com>
2025-04-14 07:01:25 +0100

    Add files via upload

---
+++ gameapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
